cdc8ac8d4c4971af916e301149eb26f08bdf52a0
Keegan <keegan@twinoaks.org>
2021-09-29 21:55:34 -0400

    title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MENTAT TRAINING
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mentat Training app goes here!
         </a>
       </header>
     </div>
